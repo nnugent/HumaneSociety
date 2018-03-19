2b85c427a5f2882ee815b8ab3b09a0ecb302e1fe
nnugent <nrnugent96@gmail.com>
2018-03-19 13:47:44 -0500

    Added database to the program.

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace HumaneSociety
 {
     public static class Query
-    {
-        
-        
-      
+    {   
+     
     }
 }
